2df41fb0e099b187176f2f51f598339404d0f751
lucelsby <lucelsby@gmail.com>
2023-12-04 15:02:09 +0000

    changing input data format of router function

--- api/routes.py
+++ api/routes.py
@@ -4,7 +4,8 @@ from core.text_extractors import extract_text, determine_input_type
 router = APIRouter()
 
 @router.post("/extract-text")
-async def extract_text_from_source(input_source: str):
+async def extract_text_from_source(request_data: dict):
+    input_source = request_data.get("input_source", "")  # Extract input_source from the request_data dictionary
     try:
         # Call your extract_text function with input_source
         extracted_text = extract_text(input_source)
